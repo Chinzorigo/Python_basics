08ff721001feee5ab3566db0237c8fb3f09b1c36
Chinzorigo <chinzorig.o@gmail.com>
2024-04-26 06:59:47 +0000

    tested cloud shell editor

--- exercises/basic_challenges/basic/create_dict.py
+++ exercises/basic_challenges/basic/create_dict.py
@@ -9,3 +9,14 @@ new_dict = {k: v for (k, v) in zip(numbers, values)}
 
 # print the dictionary
 print(new_dict)
+
+hello = 'Hello world'
+
+new_dict2 = {k: v for (k, v) in enumerate(hello)}
+
+print(new_dict2)
+
+new_dict3 = {k: v for (k, v) in enumerate(hello, start=1)}
+
+print(new_dict3)
+
